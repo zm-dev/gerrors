4ebeb88655e11c854f36832f8e03685588b88a61
taoyu <1255803549@qq.com>
2018-12-12 01:15:38 +0800

    update

--- errors.go
+++ errors.go
@@ -2,6 +2,7 @@ package gerrors
 
 import (
 	"encoding/json"
+	"net/http"
 )
 
 type GlobalError struct {
@@ -17,79 +18,94 @@ func (ge *GlobalError) Error() string {
 	return string(b)
 }
 
-func New(code int, message string, detail interface{}, statusCode int) error {
+func New(code int, statusCode int, message string, detail interface{}) error {
+	if message=="" {
+		message = http.StatusText(statusCode)
+	}
 	return &GlobalError{
-		Code:        code,
+		Code: code,
 		// ServiceName: serviceName,
-		Message:     message,
-		Detail:      detail,
-		StatusCode:  statusCode,
+		Message:    message,
+		Detail:     detail,
+		StatusCode: statusCode,
 	}
 }
 
 // BadRequest generates a 400 error.
 func BadRequest(code int, message string, detail interface{}) error {
 	return &GlobalError{
-		Code:        code,
+		Code: code,
 		// ServiceName: serviceName,
-		StatusCode:  400,
-		Message:     message,
-		Detail:      detail,
+		StatusCode: 400,
+		Message:    message,
+		Detail:     detail,
 	}
 }
 
 // Unauthorized generates a 401 error.
 func Unauthorized(code int, message string, detail interface{}) error {
 	return &GlobalError{
-		Code:        code,
+		Code: code,
 		// ServiceName: serviceName,
-		Message:     message,
-		StatusCode:  401,
-		Detail:      detail,
+		Message:    message,
+		StatusCode: 401,
+		Detail:     detail,
 	}
 }
 
 // Forbidden generates a 403 error.
-func Forbidden(code int, message string, detail interface{}) error {
+func Forbidden(code int, message string, detail ...interface{}) error {
+	if message == "" {
+		message = http.StatusText(http.StatusForbidden)
+	}
 	return &GlobalError{
-		Code:        code,
+		Code: code,
 		// ServiceName: serviceName,
-		Message:     message,
-		StatusCode:  403,
-		Detail:      detail,
+		Message:    message,
+		StatusCode: http.StatusForbidden,
+		Detail:     detail,
 	}
 }
 
 // NotFound generates a 404 error.
-func NotFound(code int, message string, detail interface{}) error {
+func NotFound(code int, message string, detail ...interface{}) error {
+	if message == "" {
+		message = http.StatusText(http.StatusNotFound)
+	}
 	return &GlobalError{
-		Code:        code,
+		Code: code,
 		// ServiceName: serviceName,
-		Message:     message,
-		StatusCode:  404,
-		Detail:      detail,
+		Message:    message,
+		StatusCode: http.StatusNotFound,
+		Detail:     detail,
 	}
 }
 
 // InternalServerError generates a 500 error.
-func InternalServerError(code int, message string, detail interface{}) error {
+func InternalServerError(code int, message string, detail ...interface{}) error {
+	if message == "" {
+		message = http.StatusText(http.StatusInternalServerError)
+	}
 	return &GlobalError{
-		Code:        code,
+		Code: code,
 		// ServiceName: serviceName,
-		Message:     message,
-		StatusCode:  500,
-		Detail:      detail,
+		Message:    message,
+		StatusCode: http.StatusInternalServerError,
+		Detail:     detail,
 	}
 }
 
 // Conflict generates a 409 error.
-func Conflict(code int, message string, detail interface{}) error {
+func Conflict(code int, message string, detail ...interface{}) error {
+	if message == "" {
+		message = http.StatusText(http.StatusConflict)
+	}
 	return &GlobalError{
-		Code:        code,
+		Code: code,
 		// ServiceName: serviceName,
-		Message:     message,
-		StatusCode:  409,
-		Detail:      detail,
+		Message:    message,
+		StatusCode: http.StatusConflict,
+		Detail:     detail,
 	}
 }
 
@@ -98,10 +114,10 @@ type ValidateError map[string][]string
 // UnprocessableEntity generates a 422 error.
 func UnprocessableEntity(code int, ve ValidateError) error {
 	return &GlobalError{
-		Code:        code,
+		Code: code,
 		// ServiceName: serviceName,
-		Message:     "The given data failed to pass validation.",
-		StatusCode:  422,
-		Detail:      ve,
+		Message:    "The given data failed to pass validation.",
+		StatusCode: http.StatusUnprocessableEntity,
+		Detail:     ve,
 	}
 }
